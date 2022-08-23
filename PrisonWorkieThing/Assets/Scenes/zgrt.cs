c6c7c3c34808137586901fd448ec5f646e28dcdd
FishyKench <m7md.al7male@gmail.com>
2022-08-23 17:25:07 +0300

    test 2 zgrt

--- PrisonWorkieThing/Assets/Scenes/zgrt.cs
+++ PrisonWorkieThing/Assets/Scenes/zgrt.cs
@@ -7,7 +7,7 @@ public class zgrt : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("zgrt");
     }
 
     // Update is called once per frame
